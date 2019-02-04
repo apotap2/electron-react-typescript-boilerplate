0d35eab7ebd028ea1c3712dfb736fe67f88e1375
alexpotapchuk <alexanderpotapchuk@gmail.com>
2019-02-04 10:38:02 +0300

    counter in reducer is inline

--- app/switchers/index.ts
+++ app/switchers/index.ts
@@ -3,12 +3,7 @@ import { routerReducer as routing } from 'react-router-redux';
 import { switcher as CounterSwitcher, TState as TCounterState } from './counter';
 
 const rootReducer = combineReducers({
-  counter: (state, action) => {
-    if (state === undefined) {
-      return CounterSwitcher.getInitialState();
-    }
-    return CounterSwitcher.apply(state, action);
-  },
+  counter: (state, action) => {if (state === undefined) {return CounterSwitcher.getInitialState();} return CounterSwitcher.apply(state, action); },
   routing: routing as Reducer<any>
 });
 
